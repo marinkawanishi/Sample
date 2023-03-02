5ca0f1ebae2d9628895cab33bb8493000fc840a2
fujit <fujit@user2>
2023-03-02 23:52:15 +0900

    Javaプログラミング入門学習終了

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        int test = 0;
+        test = 1+1;
+        System.out.println(test);
+        System.out.println("TechAcademy");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+    }
+}
+
+
