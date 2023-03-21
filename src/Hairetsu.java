544a806a17e805c2b32407adecacb41929bab8da
fujit <fujit@user2>
2023-03-21 22:40:12 +0900

    lesson4

---
+++ src/Hairetsu.java
@@ -0,0 +1,13 @@
+
+public class Hairetsu {
+
+    public static void main(String[] args) {
+        // 書き方の1つ目
+        int[] score;
+        score = new int[5];
+        // 書き方の2つ目
+        int[] score = new int[5];
+
+    }
+
+}
